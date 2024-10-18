5d2a4f8f65d0a3affd9e5ddf8faadf136534b7fe
flowercordoba <flowercordoba7@gmail.com>
2024-10-18 18:10:32 -0500

    added docker

--- src/config/database.js
+++ src/config/database.js
@@ -5,21 +5,19 @@ const env = process.env.NODE_ENV || 'development';
 // Cargar la configuración según el entorno
 const config = require(path.join(__dirname, 'config.json'))[env];
 
-// Determina si los logs deben estar habilitados
-const LOGGING_ENABLED = process.env.LOGGING_ENABLED === 'true'; // Cambia esto para activar/desactivar logs
-console.log('LOGGING_ENABLED:', LOGGING_ENABLED);
+// Crear una instancia de Sequelize con SSL habilitado
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  port: config.port,
+  dialect: config.dialect,
+  logging: config.logging || false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Si no necesitas verificar el certificado SSL
+    }
+  }
+});
 
-// Crear una instancia de Sequelize
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: config.dialect,
-    logging: LOGGING_ENABLED ? (msg) => console.log(msg) : false, // Activa el logging si está habilitado
-  });
-}
-
-// Exportar la instancia de Sequelize para usarla en los modelos
+// Exportar la instancia de Sequelize
 module.exports = sequelize;
